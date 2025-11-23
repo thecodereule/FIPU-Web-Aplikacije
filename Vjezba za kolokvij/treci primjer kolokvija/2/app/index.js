df9c314402a4faf6a10cb1accb42ac39a908c77f
Igor <igortadic.msc@gmail.com>
2025-11-23 13:12:02 +0100

    vjezba za kolokvij

---
+++ Vjezba za kolokvij/treci primjer kolokvija/2/app/index.js
@@ -0,0 +1,32 @@
+import { error } from 'console';
+import express from 'express'
+import fs from 'fs'
+import path from 'path'
+import { fileURLToPath } from 'url'
+import boatRouter from './routes/boats.js'
+import rentalsRouter from './routes/rentals.js'
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+app.use('/boats', boatRouter);
+app.use('/rentals', rentalsRouter);
+
+
+
+app.get('/', (req, res) => {
+    res.status(200).sendFile(path.join(__dirname, 'public', 'index.html'));
+})
+
+app.listen(PORT, error => {
+    if (error) {
+        console.log("Doslo je do greske kod pokretanja servera")
+    } else {
+        console.log(`Posluzitelj slusa na portu ${PORT}. Dobar dan.`)
+    }
+})
