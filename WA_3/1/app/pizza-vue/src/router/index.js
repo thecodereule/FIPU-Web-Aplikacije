26a4b1e6e9715e7116c773c937b5a8ffabaec8e2
Igor <igortadic.msc@gmail.com>
2025-12-14 19:10:43 +0100

    dodavanje pizze/:naziv route

--- WA_3/1/app/pizza-vue/src/router/index.js
+++ WA_3/1/app/pizza-vue/src/router/index.js
@@ -16,6 +16,11 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue'),
     },
+    {
+      path: '/pizze/:naziv:',
+      name: 'PizzaDetail',
+      component: PizzaDetail
+    }
   ],
 })
 
