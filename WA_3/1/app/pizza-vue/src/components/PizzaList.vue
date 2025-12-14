2a0047d349cc4cf0d6fee12bd6ed63ad6213f49f
Igor <igortadic.msc@gmail.com>
2025-12-14 12:13:52 +0100

    dodane ikone

--- WA_3/1/app/pizza-vue/src/components/PizzaList.vue
+++ WA_3/1/app/pizza-vue/src/components/PizzaList.vue
@@ -5,9 +5,10 @@
     <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
       <!-- Pizze v-for -->
       <div
-      v-for="pizza in pizze" :key="pizza.naziv"
+        v-for="pizza in pizze"
+        :key="pizza.naziv"
         :class="[
-          'bg-inherit rounded-xl overflow-hidden cursor-pointer transition-all duration300',
+          'bg-inherit rounded-xl overflow-hidden cursor-pointer transition-all duration-300',
           odabrana_pizza === pizza.naziv
             ? 'ring-4 ring-orange-300 shadow-lg shadow-orange-300/50 scale-[1.02]'
             : 'hover:scale-[1.01]',
@@ -24,51 +25,68 @@
 
         <div class="p-6">
           <div class="flex items-center space-x-3 mb-4">
-            <h2 class="text-lg font-bold text-orange-500 tracking-wide">{{pizza.naziv}}</h2>
+            <h2 class="text-lg font-bold text-orange-500 tracking-wide">{{ pizza.naziv }}</h2>
 
             <div class="flex space-x-2">
               <div
-                class="w-8 h-8 bg-orange-500 rounded-full flex items-center justify-center text-slate-50 font-semibold text-xs"
+                v-for="sastojak in pizza.sastojci"
+                class="w-12 h-12 bg-orange-500 rounded-full flex items-center justify-center text-slate-50 font-semibold text-xs"
               >
-                Icon
-              </div>
-              <div
-                class="w-8 h-8 bg-orange-500 rounded-full flex items-center justify-center text-slate-50 font-semibold text-xs"
-              >
-                Icon
-              </div>
-              <div
-                class="w-8 h-8 bg-orange-500 rounded-full flex items-center justify-center text-slate-50 font-semibold text-xs"
-              >
-                Icon
+                <v-icon :name="ikoneSastojaka[sastojak]"></v-icon>
               </div>
             </div>
           </div>
 
           <div class="space-y-2">
-            <div class="flex justify-between text-gray-700">
-              <span class="font-medium">Mala</span>
-              <span>€{{ pizza.cijene.mala }}</span>
-            </div>
-
-            <div class="flex justify-between text-gray-700">
-              <span class="font-medium">Srednja</span>
-              <span>€{{ pizza.cijene.srednja }}</span>
-            </div>
-
-            <div class="flex justify-between text-gray-700">
-              <span class="font-medium">Jumbo</span>
-              <span>€{{ pizza.cijene.jumbo }}</span>
+            <div
+              v-for="(cijena, velicina) in pizza.cijene"
+              class="flex justify-between text-gray-700"
+            >
+              <span class="font-medium">{{
+                velicina.charAt(0).toUpperCase() + velicina.slice(1)
+              }}</span>
+              <span>€{{ cijena }}</span>
             </div>
           </div>
         </div>
       </div>
-
     </div>
   </div>
 </template>
 
 <script setup>
+import { addIcons } from 'oh-vue-icons'
+import {
+  GiTomato,
+  GiCheeseWedge,
+  GiSlicedMushroom,
+  IoLeafSharp,
+  CoHotjar,
+  GiMilkCarton,
+  GiBellPepper,
+  LaPepperHotSolid,
+  GiCannedFish,
+  GiGarlic,
+  FaBacon,
+  GiHamShank,
+} from 'oh-vue-icons/icons'
+
+// registracija ikona koje ćemo koristiti
+addIcons(
+  GiTomato,
+  GiCheeseWedge,
+  GiSlicedMushroom,
+  IoLeafSharp,
+  GiBellPepper,
+  GiHamShank,
+  LaPepperHotSolid,
+  GiCannedFish,
+  GiGarlic,
+  FaBacon,
+  CoHotjar,
+  GiMilkCarton,
+)
+
 import { ref, reactive, onMounted } from 'vue'
 import axios from 'axios'
 
@@ -76,6 +94,21 @@ let odabrana_pizza = ref(null)
 let pizze = ref([])
 
 const URL_express = 'http://localhost:3000'
+
+const ikoneSastojaka = {
+  rajčica: 'gi-tomato',
+  sir: 'gi-cheese-wedge',
+  gljive: 'gi-sliced-mushroom',
+  bosiljak: 'io-leaf-sharp',
+  paprika: 'gi-bell-pepper',
+  šunka: 'gi-ham-shank',
+  'feferoni ljuti': 'la-pepper-hot-solid',
+  tunjevina: 'gi-canned-fish',
+  'crveni luk': 'gi-garlic',
+  panceta: 'fa-bacon',
+  kulen: 'co-hotjar',
+  vrhnje: 'gi-milk-carton',
+}
 
 function odaberi_pizzu(novaPizza) {
   odabrana_pizza.value = novaPizza
--- WA_3/1/app/pizza-vue/src/main.js
+++ WA_3/1/app/pizza-vue/src/main.js
@@ -1,10 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
 import './assets/tailwind.css'
+import {OhVueIcon} from 'oh-vue-icons'
 
 const app = createApp(App)
 
-app.use(router)
+app.component("v-icon", OhVueIcon)
 
 app.mount('#app')
