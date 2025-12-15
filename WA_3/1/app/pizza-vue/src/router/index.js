6e42a4e2cc59748d5a1189eb04e87b39ab8f4805
Igor <igortadic.msc@gmail.com>
2025-12-15 09:07:21 +0100

    Pregled pojedinih pizza rijesen

--- WA_3/1/app/pizza-vue/src/router/index.js
+++ WA_3/1/app/pizza-vue/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
+import PizzaDetailView from '@/views/PizzaDetailView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -17,6 +18,11 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue'),
     },
+    {
+      path: '/pizze/:naziv',
+      name: 'PizzaDetail',
+      component: PizzaDetailView
+    }
   ],
 })
 
