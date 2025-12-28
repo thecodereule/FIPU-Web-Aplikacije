b36866e97b83c7f125876a176e47a6465d3f489c
Igor <igortadic.msc@gmail.com>
2025-12-28 18:33:32 +0100

    dodavanje narudzbi sa svojstvom "ukupna_cijena"

--- WA_5/samostalni zadatak za vjezbu 5/app/pizza-express/routes/narudzbe.js
+++ WA_5/samostalni zadatak za vjezbu 5/app/pizza-express/routes/narudzbe.js
@@ -1,55 +1,82 @@
-import express from 'express';
-import { narudzbe, pizze } from '../data/data.js'; // učitavanje dummy podataka
-
+import express from "express";
 
 const router = express.Router();
 
-
 // POST /narudzbe - Izrada nove narudžbe pizza
-router.post('/', (req, res) => {
-    const { narucene_pizze, podaci_dostava } = req.body;
-    if (!narucene_pizze || narucene_pizze.length === 0) {
-        return res.status(400).json({ message: 'Nisu specificirane naručene pizze.' });
-    }
-    // Izračun ukupne cijene narudžbe
-    let ukupna_cijena = 0;
-    for (const narucena of narucene_pizze) {
-        const pizza = pizze.find(p => p.naziv.toLowerCase() ===
-            narucena.naziv.toLowerCase());
-        if (!pizza) {
-            return res.status(400).json({
-                message: `Pizza s nazivom '${narucena.naziv}' 
-nije dostupna.` });
-        }
-        const cijena = pizza.cijene[narucena.velicina.toLowerCase()];
-        if (!cijena) {
-            return res.status(400).json({
-                message: `Veličina '${narucena.velicina}' nije 
-dostupna za pizzu '${narucena.naziv}'.`
-            });
-        }
-        ukupna_cijena += cijena * narucena.kolicina;
-    }
-    ukupna_cijena = Number(ukupna_cijena.toFixed(2)); // zaokruživanje ukupne cijene na 2  decimale
+router.post("/", async (req, res) => {
+  let narudzbe_collection = req.db.collection("narudzbe");
+  let novaNarudzba = req.body;
 
+  let obavezniKljucevi = ["ime", "adresa", "telefon", "narucene_pizze"];
+  let obavezniKljuceviStavke = ["naziv", "količina", "veličina"];
 
-    const nova_narudzba = {
-        id: narudzbe.length + 1,
-        narucene_pizze,
-        ukupna_cijena,
-        podaci_dostava
-    };
-    narudzbe.push(nova_narudzba);
-    console.log(`Uspjesno kreirana narudzbe: ${nova_narudzba}`)
-    res.status(201).json({
-        message: 'Narudžba je uspješno kreirana.', narudzba:
-            nova_narudzba
-    });
+  let pizze_collection = req.db.collection("pizze");
+  let dostupne_pizze = await pizze_collection.find().toArray();
+
+  if (!obavezniKljucevi.every((kljuc) => kljuc in novaNarudzba)) {
+    return res.status(400).json({ error: "Nedostaju obavezni kljucevi" });
+  }
+
+  for (let stavka of novaNarudzba.narucene_pizze) {
+    if (!obavezniKljuceviStavke.every((kljuc) => kljuc in stavka)) {
+      return res
+        .status(400)
+        .json({ error: "Nedostaju obavezni kljcuevi u stavci narudzbe." });
+    }
+  }
+
+  if (
+    !novaNarudzba.narucene_pizze.every((stavka) => {
+      return (
+        Number.isInteger(stavka.količina) &&
+        stavka.količina > 0 &&
+        ["mala", "srednja", "velika"].includes(stavka.veličina)
+      );
+    })
+  ) {
+    return res
+      .status(400)
+      .json({ error: "Neispravni podaci u stavci narudzbe." });
+  }
+
+  if (
+    !novaNarudzba.narucene_pizze.every((stavka) =>
+      dostupne_pizze.some((pizza) => pizza.naziv === stavka.naziv)
+    )
+  ) {
+    return res
+      .status(404)
+      .json({ error: "Odabrali ste pizzu koju nemamo u ponudi" });
+  }
+
+  // izracun ukupne cijene: ukupna_cijena
+  let ukupna_cijena = 0;
+  for (let stavka of novaNarudzba.narucene_pizze) {
+    const pizza = dostupne_pizze.find((pizza) => pizza.naziv === stavka.naziv);
+
+    const cijenaZaVelicinu = pizza.cijene[stavka.veličina];
+
+    if (!cijenaZaVelicinu) {
+      return res
+        .status(400)
+        .json({ error: `Velicina ${stavka.veličina} nije dostupna za pizzu` });
+    }
+
+    ukupna_cijena += cijenaZaVelicinu * stavka.količina; 
+  }
+
+  // dodaj ukupna_cijena u narudzbu
+  novaNarudzba.ukupna_cijena = ukupna_cijena;
+
+  try {
+    let result = await narudzbe_collection.insertOne(novaNarudzba);
+    res
+      .status(201)
+      .json({ insertedId: result.insertedId, ukupna_cijena: ukupna_cijena });
+  } catch (error) {
+    console.log(error.errorResponse);
+    res.status(400).json({ error: error.errorResponse });
+  }
 });
-
-
-router.get('/', (req, res) => {
-    return res.status(200).json(narudzbe);
-})
 
 export default router;
